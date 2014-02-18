987a638ac78cd5fc4fff681173bacac73105fc74
kylehild <kylehild@umich.edu>
2014-02-17 21:27:28 -0500

    Changed 2 bombs to have a frequency also
    Added 3 bomb probability too

---
+++ Assets/Scripts/MenuBarScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuBarScript : MonoBehaviour {
+	
+	public GUISkin		fontSkin;
+	public GameObject	player;
+	
+	private int		desiredWidth = 360;
+	private int		desiredHeight = 315;
+	private float	rW, rH;
+	
+	void OnGUI () {
+		
+		rW = (float) Screen.width / (float) desiredWidth;
+		rH = (float) Screen.height / (float) desiredHeight;
+		
+		GUI.skin = fontSkin;
+		GUI.Label (new Rect (rW*40, rH*0, 200, 100), "Score");
+		GUI.Label (new Rect (rW*40, rH*10, 200, 100), player.GetComponent<PlayerController>().score.ToString("00000"));
+	}
+}
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,12 @@ public class PlayerController : MonoBehaviour {
 
 	public float 		score = 0;
 	public float		additions = 0;
-	public float		frequency = 5f;
+	public float		frequency = 50f;
+	public float		frequency2 = 10f;
+	public float		frequency3 = 8f;
 	public float		numFrames = -1f;
+	public float		numFrames2 = 0f;
+	public float		numFrames3 = 0f;
 	public GameObject	bomb;
 
 	// Use this for initialization
@@ -18,15 +22,31 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 
 		if(score < 25f) frequency = 50f;
-		else if(score < 50f) frequency = 40f;
-		else if(score < 100f) frequency = 30f;
+		else if(score < 100f) frequency = 40f;
+		else if(score < 200f) frequency = 30f;
+
+		if(score < 75f) frequency2 = 10f;
+		if(score < 150f) frequency2 = 7f;
 
 		if(numFrames < frequency && numFrames >= 0f) numFrames++;
 		else if(numFrames == frequency){
 			numFrames = 0f;
 			Instantiate(bomb);
-			if(score > 75f)
-				Instantiate(bomb);
+			if(score > 50f){
+				numFrames2++;
+				if(numFrames2 == frequency2){
+					numFrames2 = 0f;
+					Instantiate(bomb);
+				}
+			}
+
+			if(score > 125f){
+				numFrames3++;
+				if(numFrames3 == frequency3){
+					numFrames3 = 0f;
+					Instantiate(bomb);
+				}
+			}
 		}
 
 		score = (int) transform.position.y / (-10) + additions;
