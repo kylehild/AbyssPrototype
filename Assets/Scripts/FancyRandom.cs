8811bf763283e11c3c1a1319a4df2724fc0cdb02
Kyle Hildebrandt <kylehild@umich.edu>
2014-03-20 14:00:19 -0500

    Finished documentation for every function

--- Assets/Scripts/FancyRandom.cs
+++ Assets/Scripts/FancyRandom.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 
 public class FancyRandom : MonoBehaviour {
 
+	//returns a value along a normal gaussian distribution with a 
+	//mean of 0 and standard deviation of 1
 	private static float NextGaussian(){
 		float first = Random.value;
 		float second = Random.value;
@@ -11,6 +13,9 @@ public class FancyRandom : MonoBehaviour {
 					Mathf.Sin(2.0f * Mathf.PI * second);
 	}
 
+	//returns a value with a mean centered directly between min and max and a standard deviation
+	//of 1/6 the distance between min and max to fit the Normal almost completely between min and max
+	//Also sets values outside range to bound values
 	public static float NextNormalInRange(float min, float max){
 		if(min > max) max = min;
 
@@ -25,6 +30,9 @@ public class FancyRandom : MonoBehaviour {
 		return val;
 	}
 
+	//returns a value that follows a normal distribution with the mean and stdev set
+	//If min and max are set, the function takes any value lower than min and sets it to min
+	//while anything higher than max gets set to max
 	public static float NextNormalCentered(float mean, float stdev, 
 	             	float min = float.MinValue, float max = float.MaxValue){
 		if(min > max) max = min;
@@ -39,6 +47,8 @@ public class FancyRandom : MonoBehaviour {
 		return val;
 	}
 
+	//returns a value along a triangular curve with min and max set and the peak
+	//of the distribution set at mode
 	public static float NextTriangular(float min, float max, float mode){
 		if(min > max) max = min;
 
@@ -50,19 +60,25 @@ public class FancyRandom : MonoBehaviour {
 			return max - Mathf.Sqrt((1 - val) * (max - min) * (max - mode));
 	}
 
+	//returns a float with equal probability along a uniform distribution with the smallest
+	//possible value of min and largest of max inclusive
 	public static float NextUniform(float min, float max){
 		return Random.Range (min, max);
 	}
 
+	//returns an int with equal probability along a uniform distribution with the smallest
+	//possible value of min and largest of max inclusive
 	public static int NextUniform(int min, int max){
-		return Random.Range (min, max);
+		return Random.Range (min, max+1);
 	}
 
+	//returns a random boolean value with equal chance true and false
 	public static bool NextBool(){
-		float rand = Random.Range (0, 2);
+		float rand = NextUniform (0, 1);
 		return rand == 1;
 	}
 
+	//returns a random entry in an array with equal chance for any entry to be picked
 	public static T NextInArray<T>(T[] array){
 		return array [Random.Range (0, array.Length)];
 	}
