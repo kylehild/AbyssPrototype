cc4121afa6f7afa5c01b6d3777a48e223c755d90
Kyle Hildebrandt <kylehild@umich.edu>
2014-03-20 12:00:56 -0500

    Made Centered function

--- Assets/Scripts/FancyRandom.cs
+++ Assets/Scripts/FancyRandom.cs
@@ -1,15 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class FancyRandom : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private float NextGaussian(){
+		float first = Random.value;
+		float second = Random.value;
+		return Mathf.Sqrt(-2.0 * Mathf.Log(first)) * 
+					Mathf.Sin(2.0 * Mathf.PI * second);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public float NextNormalInRange(){
+
+	}
+
+	public float NextNormalCentered(float mean, float stdev, 
+	             	float min = float.MinValue, float max = float.MaxValue){
+		float val = mean + stdev * NextGaussian();
+
+		if(min > float.MinValue && val < min)
+			val = min;
+		else if(max > float.MaxValue && val > max)
+			val = max;
+
+		return val;
+	}
+
+	public float NextTriangular(){
+
+	}
+
+	public float NextUniform(){
+
+	}
+
+	public bool NextBool(){
+
+	}
+
+	public T NextInArray(T[] array){
+		
 	}
 }
